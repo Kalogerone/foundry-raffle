7905f568b12f26e103383db5b7b241a8f1eb3e9e
Kalogerone <george.kalog@hotmail.com>
2023-11-30 16:42:21 +0200

    added deploy scripts and tests

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -68,7 +68,7 @@ contract Raffle is VRFConsumerBaseV2 {
     }
 
     function enterRaffle() external payable {
-        if (msg.value <= i_entranceFee) {
+        if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthSent();
         }
         s_players.push(payable(msg.sender));
@@ -139,5 +139,13 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function getRaffleState() external view returns (RaffleState) {
+        return s_raffleState;
+    }
+
+    function getPlayer(uint256 indexOfPlayer) external view returns (address) {
+        return s_players[indexOfPlayer];
     }
 }
